bf09bcb81a48cb46b9e0aacc62a965963bf10388
Jonathan Levy <jonathan.levy@powerhrg.com>
2026-03-02 03:22:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+//Looing Lab
+
+function writeCards(names, event) {//returns an array of thank you messages for each name provided to the function
+  let message = [];
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return message;
+}
+
+function countDown(number) {//invokes console.log once for each number, counting down from the number provided to zero
+    while (number >= 0) {
+        console.log(number);
+        debugger;
+        number--;
+    }
+}
